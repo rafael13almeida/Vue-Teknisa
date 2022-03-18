0a8b5947b0f0331d5a81a206d5d494201fe0fc13
Rafael Almeida <rafael13almeida@gmail.com>
2022-03-18 11:47:35 -0300

    Atualizando cards e modal

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VModal from 'vue-js-modal'
 
 Vue.config.productionTip = false
+Vue.use(VModal)
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
